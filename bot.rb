97c2fc6e0edaf01efbcf683ee3c8d100e02dc2b0
ZainW <zainthegreatcb@gmail.com>
2017-03-22 10:21:36 -0400

    remove figaro

--- bot.rb
+++ bot.rb
@@ -1,5 +1,5 @@
 require 'discordrb'
-require 'figaro'
+require 'yaml'
 require 'pry'
 
 require_relative 'config.rb'
@@ -7,6 +7,6 @@ require_relative 'config.rb'
 CONFIG = Config.new('config/application.yml')
 
 
-bot = Discordrb::Commands::CommandBot.new token: CONFIG.TOKEN, client_id: CONFIG.CLIENTID, prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: CONFIG.TOKEN, client_id: CONFIG.CLIENTID, prefix: 'w'
 
 bot.run
